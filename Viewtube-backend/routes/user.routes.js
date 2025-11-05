ac33be46e82ed264bf6f0bcbb7220f3084257b2b
Gursahib Singh <aulakhg808@gmail.com>
2025-11-05 18:03:16 +0530

    user routes updated

---
+++ Viewtube-backend/routes/user.routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import authMiddleware from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+// User routes can be added here (register, login, profile, etc.)
+
+export default router;
